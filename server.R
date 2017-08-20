eaab0fb9abf7391259792a7dd74e0b3c34e8a858
nattsp <drnphillips@gmail.com>
2017-08-20 18:39:17 +1000

    shiny app works locally

---
+++ server.R
@@ -0,0 +1,89 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+
+library(shiny)
+library(dplyr)
+library(ggplot2)
+
+load(".\\data\\immDataAll.Rda")
+load(".\\data\\immDataRegion.Rda")
+
+# Logic for drawing plot
+shinyServer(function(input, output) {
+
+    output$distPlot <- renderPlot({
+        
+        if (input$vacc=='PCT_DTP'){
+            plotData <- immDataAll %>% 
+                select(PCT_DTP, PCT_Fully) %>%
+                filter(PCT_DTP > 0)
+        } else if (input$vacc=="PCT_Polio"){
+            plotData <- immDataAll %>% 
+                select(PCT_Polio, PCT_Fully) %>%
+                filter(PCT_Polio > 0)
+        } else if (input$vacc=="PCT_HIB") {
+            plotData <- immDataAll %>% 
+                select(PCT_HIB, PCT_Fully) %>%
+                filter(PCT_HIB > 0)
+        } else if (input$vacc=="PCT_HEP") {
+            plotData <- immDataAll %>% 
+                select(PCT_HEP, PCT_Fully) %>%
+                filter(PCT_HEP > 0)
+        } else if (input$vacc=="PCT_MMR") {
+            plotData <- immDataAll %>% 
+                select(PCT_MMR, PCT_Fully) %>%
+                filter(PCT_MMR > 0)
+        } else if (input$vacc=="PCT_Pneumo") {
+            plotData <- immDataAll %>% 
+                select(PCT_Pneumo, PCT_Fully) %>%
+                filter(PCT_Pneumo > 0)
+        } else if (input$vacc=="PCT_MenC") {
+            plotData <- immDataAll %>% 
+                select(PCT_MenC, PCT_Fully) %>%
+                filter(PCT_MenC > 0)
+        } else {
+            plotData <- immDataAll %>% 
+                select(PCT_Varicella, PCT_Fully) %>%
+                filter(PCT_Varicella > 0)
+        }
+        names(plotData)[1] <- "x"
+
+        #plot(immDataAll$PCT_DTP, immDataAll$PCT_Fully,
+        plot(plotData$x,
+             plotData$PCT_Fully,
+             
+             main="Total vaccination rate against individual vaccinations",
+             ylab = "Total vaccination rate",
+             
+             xlab = if (input$vacc=='PCT_DTP'){
+                 xlab = "Diphtheria tetanus and pertussis whooping cough"
+             } else if (input$vacc=="PCT_Polio"){
+                 xlab = "Polio"
+             } else if (input$vacc=="PCT_HIB") {
+                 xlab = "Haemophilus influenzae type b causes meningitis"
+             } else if (input$vacc=="PCT_HEP") {
+                 xlab = "Hepatitis"
+             } else if (input$vacc=="PCT_MMR") {
+                 xlab = "Measles-mumps-rubella vaccine"
+             } else if (input$vacc=="PCT_Pneumo") {
+                 xlab = "Pneumonia lung infection"
+             } else if (input$vacc=="PCT_MenC") {
+                 xlab = "Meningococcal C"
+             } else {
+                 xlab = "Varicella chickenpox"
+             } 
+             )
+        
+    })
+    
+    output$distPlot2 <- renderPlot({
+        ggplot(immDataRegion, aes(x=Time_Period, y=Percent, 
+                                  colour = Region_Type, group=Region_Type)) +
+            geom_line() +
+            labs( title = "Variation of full immunisation rates for each zone across time")
+            #theme(plot.title = element_text(family = "Trebuchet MS", face="bold", size=14, hjust=0))
+        
+    })
+})
---
+++ ui.R
@@ -0,0 +1,54 @@
+#
+# Shiny application for Immunisation Rates
+# 
+#    https://nattsapps.shinyapps.io/ImmunisationRates/
+#
+# Natalie Phillips
+#
+
+library(shiny)
+shinyUI(fluidPage(
+    titlePanel("Immunisations Rates for Australia"),
+    sidebarLayout(
+        sidebarPanel(
+            h3("Plot 1"),
+            h3("Choose which vaccinations to see"),
+            
+            p("Some vaccinations are more closely aligned with the total vaccination
+              rate than others. Ignoring time and region you have a choice of vaccinate
+              rates to compare to total vaccination rates in Australia."),
+            
+            p("Choose from one of the 8 seperate vaccinations that are tracked:"),
+            
+            radioButtons("vacc", "Vaccine to compare with total:",
+                         c("Diphtheria, tetanus and pertussis (whooping cough)" = "PCT_DTP",
+                           "Polio" = "PCT_Polio",
+                           "Haemophilus influenzae type b (causes meningitis)" = "PCT_HIB",
+                           "Hepatitis" = "PCT_HEP",
+                           "Measles-mumps-rubella vaccine" = "PCT_MMR",
+                           "Pneumonia (lung infection) " = "PCT_Pneumo",
+                           "Meningococcal C" = "PCT_MenC",
+                           "Varicella (chickenpox)" = "PCT_Varicella")),
+            
+            h3("Plot 2"),
+            
+            h3("Changing rate of fully vaccinated children across Australia"),
+            
+            p("Vaccination rates vary across time. The second plot shows a time series analysis
+              of different regions in Australia and their changing vaccination rates. This graph
+              is not interactive however I hope it is informative none the less."),
+            
+            p("The second plot looks better if you have a big screen available.")
+        ),
+        
+        # Show plot of immunisation rates
+        
+        mainPanel(
+            h2("Vaccination rates"),
+            
+            plotOutput('distPlot'),
+            
+            plotOutput('distPlot2')
+        )
+    )
+))
